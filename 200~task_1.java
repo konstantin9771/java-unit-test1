3e3eaf0c4eca011869c85e1807015cfdc52822f8
Konstantin Razumov <konstantin9771@gmail.com>
2025-06-19 20:26:47 +0300

    del autotests

--- 200~task_1.java
+++
@@ -1,7 +0,0 @@
-@Test
-public void checkIsAdultWhenAgeIsMoreThan18True() {
-    Program program = new Program();
-    boolean isAdult = program.checkIsAdult(19);
-    boolean expected = true;
-	assertEquals("Person's age is equal to or more than 18", expected, isAdult);// Напиши код здесь
-}
